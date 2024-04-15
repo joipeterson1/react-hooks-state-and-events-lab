b674a27c6e80e162b82960b9a8daefa04f6bfd34
Dalecia <petersondalecia@gmail.com>
2024-04-14 20:14:57 -0400

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState('');
+  const cartList = cart ? "in-cart" : '';
+  const handleCartButtonClick = () => {
+    // toggle the cart state when the button is clicked
+    setCart(!cart);
+  };
+
   return (
-    <li className="">
+    <li className={cartList}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartButtonClick}>
+        {cart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+const [filterBy, setFilterBy] = useState('All')
+
+function handleSelectedCategory(event){
+  setFilterBy(event.target.value)
+}
+
+const categoryDisplay = items.filter((item) => {
+  if (filterBy === 'All'){
+    return true
+
+  } else {
+    return item.category === filterBy
+  }
+  })
+
+  const selectedCategory = categoryDisplay.map((item)=> {
+    return(
+      <Item key={item.id} name={item.name} category={item.category}/>
+    )
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange = {handleSelectedCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +34,15 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {selectedCategory}
       </ul>
     </div>
   );
 }
 
 export default ShoppingList;
+/*
+{items.map((item) => (
+  <Item key={item.id} name={item.name} category={item.category} />
+))}
+*/
